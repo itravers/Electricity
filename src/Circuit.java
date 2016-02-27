16fd61f57b08273d1437bea89cb6192b12cd539e
Isaac Assegai <isaac.a.travers@gmail.com>
2016-02-26 20:29:28 -0800

    adding methods to circuit

--- src/Circuit.java
+++ src/Circuit.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 
+
 /**
  * A circuit represents an actual electrical circuit.
  * We model ours with nodes, resistances and power supplies.
@@ -10,7 +11,36 @@ import java.util.ArrayList;
  */
 public class Circuit {
 	/* Member Variables */
-	ArrayList<Circuit>circuits; // In order to simplify circuits we will have to remember the previous ones.
-	ArrayList<Node>nodes; // A List of all the nodes in the circuit.
-	ArrayList<Resistor>resistors; // An list of all the resistors in the circuit.
+	private ArrayList<Circuit>circuits; // In order to simplify circuits we will have to remember the previous ones.
+	private ArrayList<Node>nodes; // A List of all the nodes in the circuit.
+	private ArrayList<Resistor>resistors; // An list of all the resistors in the circuit.
+	private PowerSupply supply; // The power supply of the circuit.
+	
+	/** Constructor
+	 * Create a new empty circuit.
+	 */
+	public Circuit(){
+		circuits = new ArrayList<Circuit>();
+		nodes = new ArrayList<Node>();
+		resistors = new ArrayList<Resistor>();
+		supply = new PowerSupply();
+	}
+	
+	/* Public Methods */
+	public void addCircuit(Circuit c){
+		circuits.add(c);
+	}
+	
+	public void addNode(Node n){
+		nodes.add(n);
+	}
+	
+	public void addResistor(Resistor r){
+		resistors.add(r_;)
+	}
+	
+	public void addSupply(PowerSupply s){
+		supply = s;
+	}
+	
 }
