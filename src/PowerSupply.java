4e985af991c2e8e8bcfda0ff3aed6c41b4ae8d84
Isaac Assegai <isaac.a.travers@gmail.com>
2016-02-26 21:03:33 -0800

    working on ps

--- src/PowerSupply.java
+++ src/PowerSupply.java
@@ -1,3 +1,6 @@
+import java.math.BigInteger;
+import java.util.Random;
+
 /**
  * A power supply supplies power to a circuit.
  * It has a name, voltage, +node and -node.
@@ -6,4 +9,33 @@
  */
 public class PowerSupply {
 	String name;
+	Double voltage;
+	String posNode;
+	String negNode;
+	
+	/**
+	 * Create a new unconnected power supply.
+	 */
+	public PowerSupply(){
+		name = newName(System.currentTimeMillis());
+		voltage = null;
+		posNode = null;
+		negNode = null;
+	}
+	
+	/* public methods. */
+	public void connectPosNode(String n){
+		posNode = n;
+	}
+	
+	public void connectNegNode(String n){
+		negNode = n;
+	}
+	
+	
+	/* Private Methods */
+	private String newName(long l){
+		Random random = new Random(l);
+		return new BigInteger(130, random).toString(32);
+	}
 }
